7d1dade73908bf927c4423bfa4b74d1d25684ed8
Euan Jed Tabamo <estabamo1@up.edu.ph>
2025-02-18 14:01:30 +0800

    feat: add ability to validate passwords

---
+++ index.js
@@ -0,0 +1,43 @@
+/* Tabamo, Euan Jed S.
+2023-10822
+February 18, 2025
+CMSC 100 C1L
+*/
+
+function validatePassword(password1, password2) {
+  // Passwords must match
+  if (password1 !== password2) {
+    return false;
+  }
+
+  // Passwords have at least 8 characters
+  if (password1.length < 8 || password2.length < 8) {
+    return false;
+  }
+
+  // Passwords must have at least 1 number, 1 uppercase character, and 1 lowercase character
+  let hasNumber = false;
+  let hasUppercase = false;
+  let hasLowercase = false;
+  for (let i = 0; i < password2.length; i++) {
+    // Get the char at pos i
+    const character = password2.charAt(i);
+
+    // Parse the char as int and check if it is the same char by using comparison
+    if (parseInt(character) == character) {
+      hasNumber = true;
+    }
+    // Transform the char to uppercase. If the result is the same, then the char is uppercase 
+    else if (character.toUpperCase() === character) {
+      hasUppercase = true;
+    }
+    // Transform the char to lowercase. If the result is the same, then the char is lowercase 
+    else if (character.toLowerCase() === character) {
+      hasLowercase = true;
+
+    }
+  }
+
+  // Check remaining conditions
+  return hasNumber && hasUppercase && hasLowercase;
+}
